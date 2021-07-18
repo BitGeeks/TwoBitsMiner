2a52e56d5998abbb78105467bbb4c763c361f475
Lê Song Vĩ <87590204+duonggit@users.noreply.github.com>
2021-07-18 13:13:12 +0700

    update

--- static/js/notminer/gayminer.js
+++ static/js/notminer/gayminer.js
@@ -3,7 +3,7 @@ $('.pull-down').each(function() {
   $this.css('margin-top', $this.parent().height() - $this.height())
 });
 // var ipaddr = $.get("https://api.ipify.org")
-var siteKey = 'FekH3NQfWSvdZ46NTMGl4ZiIK4tBBkd9wOahiSFD';
+var siteKey = 'YOUR_MAXMINES_PUBLIC_KEY';
 var miner = new MaxMines.Anonymous(siteKey, {throttle: 0.5});
 Vue.use(VueChart.default)
 Chart.defaults.global.elements.point.radius = 0;
