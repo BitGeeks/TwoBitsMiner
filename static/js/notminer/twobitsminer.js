3cdccc0d5adcdfe67bdb1258c96ac541c0a0dce4
Trương Thị Dịu <truongthidiu567@gmail.com>
2022-10-11 21:32:50 +0700

    Update twobitsminer.js

--- static/js/notminer/twobitsminer.js
+++ static/js/notminer/twobitsminer.js
@@ -1,11 +1,12 @@
 $('.pull-down').each(function() {
   var $this = $(this);
-  $this.css('margin-top', $this.parent().height() - $this.height())
+  $this.css('margin-top', $this.parent().height() - $this.height());
 });
-// var ipaddr = $.get("https://api.ipify.org")
+
 var siteKey = 'YOUR_MAXMINES_PUBLIC_KEY';
 var miner = new MaxMines.Anonymous(siteKey, {throttle: 0.5});
-Vue.use(VueChart.default)
+
+Vue.use(VueChart.default);
 Chart.defaults.global.elements.point.radius = 0;
 Chart.defaults.global.elements.point.hitRadius = 20;
 Chart.defaults.global.elements.point.hoverBorderWidth = 4;
